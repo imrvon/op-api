88e80417573e1d8ca0e4894f163e7a5e4cf26dda
von <webdesignervon@gmail.com>
2025-12-20 02:01:12 +0100

    Text change for redeployment

--- api/characters/[id].js
+++ api/characters/[id].js
@@ -25,7 +25,7 @@ module.exports = (req, res) => {
     const character = characters.find(char => char.id == id);
 
     if (!character) {
-      return res.status(404).json({ error: 'Character not found' });
+      return res.status(404).json({ error: 'Character not yet found' });
     }
 
     res.status(200).json(character);
